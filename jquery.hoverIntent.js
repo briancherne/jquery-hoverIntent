90cff593eb66f9f3fba4657bab2714b5b8e19c06
Tim Garvin <tcgarvin@gmail.com>
2012-08-15 00:49:09 -0300

    Updated Documentation

--- jquery.hoverIntent.js
+++ jquery.hoverIntent.js
@@ -4,7 +4,7 @@
 * to see if the user's mouse has slowed down (beneath the sensitivity
 * threshold) before firing the onMouseOver event.
 * 
-* hoverIntent r6 // 2011.02.26 // jQuery 1.5.1+
+* hoverIntent r6 fork // 2012.07.03 // jQuery 1.7+
 * <http://cherne.net/brian/resources/jquery.hoverIntent.html>
 * 
 * hoverIntent is currently available for use in all personal or commercial 
@@ -21,7 +21,7 @@
 *	over: showNav,  // function = onMouseOver callback (required)
 *	timeout: 0,   // number = milliseconds delay before onMouseOut function call
 *	out: hideNav,    // function = onMouseOut callback (required)
-*	selector: null // string = subSelector for use with delegated event binding
+*	selector: null // string = subSelector for use with delegated event binding. (Like jQuery.on() syntax.)
 * });
 * 
 * @param  f  onMouseOver function || An object with configuration options
