83d287767268ded250eb5a9a26f1509d7a462368
Brian Cherne <brian@cherne.net>
2014-06-29 18:14:18 -0400

    Changed to symantic versioning (from r8 to v1.8.0)

--- jquery.hoverIntent.js
+++ jquery.hoverIntent.js
@@ -1,5 +1,5 @@
 /*!
- * hoverIntent r8 // 2014.06.29 // jQuery 1.9.1+
+ * hoverIntent v1.8.0 // 2014.06.29 // jQuery v1.9.1+
  * http://cherne.net/brian/resources/jquery.hoverIntent.html
  *
  * You may use hoverIntent under the terms of the MIT license. Basically that
